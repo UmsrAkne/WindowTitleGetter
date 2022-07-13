caa3c5af383d20fddc343f82d54f22eac895d7b1
Akn <sibata486@gmail.com>
2022-07-13 15:26:37 +0900

    init

---
+++ WindowTitleGetter/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace WindowTitleGetter.ViewModels
+{
+    using Prism.Mvvm;
+
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Windot title getter";
+
+        public MainWindowViewModel()
+        {
+        }
+
+        public string Title
+        {
+            get => title;
+            set => SetProperty(ref title, value);
+        }
+    }
+}
