817e35dc90cca9a4024d08ddb92f1b6eb169edd3
Akn <sibata486@gmail.com>
2022-07-13 15:46:25 +0900

    add / ウィンドウのタイトル等の情報をまとめておくためのクラスを追加

--- WindowTitleGetter/ViewModels/MainWindowViewModel.cs
+++ WindowTitleGetter/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,19 @@
 ﻿namespace WindowTitleGetter.ViewModels
 {
+    using System.Collections.ObjectModel;
     using System.Diagnostics;
     using System.Linq;
     using Prism.Mvvm;
+    using WindowTitleGetter.Models;
 
     public class MainWindowViewModel : BindableBase
     {
         private string title = "Windot title getter";
+        private ObservableCollection<WindowInfo> windows;
 
         public MainWindowViewModel()
         {
+            GetWindowTitleList();
         }
 
         public string Title
@@ -18,14 +22,16 @@
             set => SetProperty(ref title, value);
         }
 
+        public ObservableCollection<WindowInfo> Windows { get => windows; set => SetProperty(ref windows, value); }
+
         private void GetWindowTitleList()
         {
             var processes = Process.GetProcesses();
+            var windowInfos = processes
+                .Where(process => process.MainWindowTitle.Length != 0)
+                .Select(process => new WindowInfo() { Title = process.MainWindowTitle });
 
-            foreach (var p in processes.Where(process => process.MainWindowTitle.Length != 0))
-            {
-                System.Diagnostics.Debug.WriteLine($"MainWindowViewModel : {p.MainWindowTitle}");
-            }
+            Windows = new ObservableCollection<WindowInfo>(windowInfos);
         }
     }
 }
