a3453799b338941f057a44b3574bb947eb5058b4
Akn <sibata486@gmail.com>
2022-07-16 23:14:35 +0900

    add / ListBox の日時を表示する項目にコンバーターを適用

---
+++ WindowTitleGetter/Models/DateTimeConverter.cs
@@ -0,0 +1,20 @@
+﻿namespace WindowTitleGetter.Models
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime dateTime = (DateTime)value;
+            return dateTime != default(DateTime) ? dateTime.ToString("MM/dd HH:mm") : "----- -----";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
